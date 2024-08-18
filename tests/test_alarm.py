22108c94d9be14edfea904880b478153f6ce8b00
Ilia Sotnikov <hostcc@gmail.com>
2024-08-18 14:18:31 +0300

    Ignore device-related exception when simulating alerts from history
    * `G90Alarm._simulate_alerts_from_history()` method now ignores
      device-related exceptions skipping to next cycle - those could be
      timeouts etc. Other exceptions will be reported and simulating will be
      aborted. The method now also properly handles device history being
      empty, it just skips to next polling cycle

--- tests/test_alarm.py
+++ tests/test_alarm.py
@@ -1,6 +1,7 @@
 import asyncio
 import sys
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, DEFAULT
+from itertools import cycle
 import pytest
 sys.path.extend(['src', '../src'])
 from pyg90alarm import (   # noqa:E402
@@ -26,6 +27,9 @@ from pyg90alarm.user_data_crc import (   # noqa:E402
 )
 from pyg90alarm.config import (  # noqa: E402
     G90AlertConfigFlags,
+)
+from pyg90alarm.exceptions import (  # noqa: E402
+    G90TimeoutError,
 )
 
 
@@ -493,6 +497,8 @@ async def test_history(mock_device):
 
 
 @pytest.mark.g90device(sent_data=[
+    # Simulate empty history initially
+    b'ISTART[200,[[0,0,0]]]IEND\0',
     # The history records will be used to remember the timestamp of most recent
     # one
     b'ISTART[200,[[1,1,1],'
@@ -525,10 +531,17 @@ async def test_simulate_alerts_from_history(mock_device):
     g90 = G90Alarm(host=mock_device.host, port=mock_device.port)
     g90.alarm_callback = alarm_cb
     g90.armdisarm_callback = armdisarm_cb
-    # Simulate device notifications from the history data above
-    await g90.start_simulating_alerts_from_history()
-    # Both callbacks should be called, wait for that
-    await asyncio.wait([future_alarm, future_armdisarm], timeout=0.1)
+    # Simulate device timeout exception every 2nd call to `G90Alarm.history()`
+    # method - the processing should still result in callbacks invoked
+    g90.history = MagicMock(wraps=g90.history)
+    g90.history.side_effect = cycle([G90TimeoutError, DEFAULT])
+    # Simulate device notifications from the history data above, small interval
+    # is set to shorten the test run time
+    await g90.start_simulating_alerts_from_history(interval=0.1)
+    # Both callbacks should be called, wait for that - the timeout should be
+    # sufficient for extra iterations in the method under test, to accommodate
+    # the simulated exceptions above
+    await asyncio.wait([future_alarm, future_armdisarm], timeout=0.5)
     # Stop simulating the alert from history
     await g90.stop_simulating_alerts_from_history()
 
